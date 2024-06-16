3d988cedca276a1ceaf8b90bf3fc5a56ef253ea4
Leschismoreptyltd <ddlesch88@gmail.com>
2024-06-16 16:39:17 +0200

    First Production Build

---
+++ dist/assets/AboutUs-Bc50ZxtH.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-Cdcet8QQ.js";const e=()=>s.jsx(s.Fragment,{children:s.jsx("h1",{children:"This is the AboutUs view"})});export{e as default};
---
+++ dist/assets/ContactUs-B4ypQ-7L.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Cdcet8QQ.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx("h1",{children:"This is the Contact Us View"})});export{e as default};
